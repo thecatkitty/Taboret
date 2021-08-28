3c33e0118b98076599e268019d986af8fee77942
Mateusz Karcz <mateusz.karcz@interia.eu>
2021-08-29 01:32:22 +0200

    Strona główna

---
+++ KucykoweRodeo/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using KucykoweRodeo.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace KucykoweRodeo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ArchiveContext _context;
+
+        public IndexModel(ILogger<IndexModel> logger, ArchiveContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public ArchiveContext Context => _context;
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
