2823fd9e6906968416152d941b3dfa41feb35939
Mateusz Karcz <mateusz.karcz@interia.eu>
2021-10-16 13:59:56 +0200

    Dodaj tworzenie artykułów

--- KucykoweRodeo/Controllers/ArticlesController.cs
+++ KucykoweRodeo/Controllers/ArticlesController.cs
@@ -21,22 +21,41 @@ namespace KucykoweRodeo.Controllers
             _context = context;
         }
 
-#if false
         // POST: ArticlesController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        [Authorize]
+        public async Task<IActionResult> Create([Bind("Title,IssueSignature,Page,Subject,CategoryId,Lead,WordCount")] Article article, string authors, string tags)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            if (!ModelState.IsValid) return RedirectToAction("Index", "Issues");
+
+            article.OrdinalNumber = _context.Articles
+                .AsQueryable()
+                .Where(a => a.IssueSignature == article.IssueSignature)
+                .Max(a => a.OrdinalNumber) + 1;
+
+            article.Authors ??= new HashSet<Author>();
+            article.Tags ??= new HashSet<Tag>();
+
+            var (knownAuthors, unknownAuthors) = _context.GetAuthors(authors);
+            _context.Authors.AddRange(unknownAuthors);
+            knownAuthors.ForEach(author => article.Authors.Add(author));
+            unknownAuthors.ForEach(author => article.Authors.Add(author));
+
+            var (knownTags, unknownTags) = _context.GetTags(tags);
+            _context.Tags.AddRange(unknownTags);
+            knownTags.ForEach(tag => article.Tags.Add(tag));
+            unknownTags.ForEach(tag => article.Tags.Add(tag));
+
+            _context.Articles.Add(article);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(
+                nameof(IssuesController.Edit),
+                "Issues",
+                new { id = article.IssueSignature },
+                article.Id.ToString());
         }
-#endif
 
         // POST: ArticlesController/Edit/5
         [HttpPost]
@@ -49,68 +68,65 @@ namespace KucykoweRodeo.Controllers
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) return RedirectToAction("Index", "Issues");
+
+            try
             {
-                try
-                {
-                    var article = _context.Articles
-                        .Include(a => a.Authors)
-                        .Include(a => a.Tags)
-                        .First(a => a.Id == id);
+                var article = _context.Articles
+                    .Include(a => a.Authors)
+                    .Include(a => a.Tags)
+                    .First(a => a.Id == id);
 
-                    article.Title = input.Title ?? article.Title;
-                    article.Subject = input.Subject ?? article.Subject;
-                    article.Lead = input.Lead ?? article.Lead;
+                article.Title = input.Title ?? article.Title;
+                article.Subject = input.Subject ?? article.Subject;
+                article.Lead = input.Lead ?? article.Lead;
                     
-                    if (input.Page != 0) article.Page = input.Page;
-                    if (input.CategoryId != 0) article.CategoryId = input.CategoryId;
-                    if (input.WordCount != 0) article.WordCount = input.WordCount;
+                if (input.Page != 0) article.Page = input.Page;
+                if (input.CategoryId != 0) article.CategoryId = input.CategoryId;
+                if (input.WordCount != 0) article.WordCount = input.WordCount;
                     
-                    var (knownAuthors, unknownAuthors) = _context.GetAuthors(authors);
-                    _context.Authors.AddRange(unknownAuthors);
-                    unknownAuthors.ForEach(author => knownAuthors.Add(author));
+                var (knownAuthors, unknownAuthors) = _context.GetAuthors(authors);
+                _context.Authors.AddRange(unknownAuthors);
+                unknownAuthors.ForEach(author => knownAuthors.Add(author));
 
-                    article.Authors
-                        .Where(author => !knownAuthors.Contains(author))
-                        .ToList()
-                        .ForEach(author => article.Authors.Remove(author));
-                    knownAuthors
-                        .Where(author => !article.Authors.Contains(author))
-                        .ToList()
-                        .ForEach(author => article.Authors.Add(author));
+                article.Authors
+                    .Where(author => !knownAuthors.Contains(author))
+                    .ToList()
+                    .ForEach(author => article.Authors.Remove(author));
+                knownAuthors
+                    .Where(author => !article.Authors.Contains(author))
+                    .ToList()
+                    .ForEach(author => article.Authors.Add(author));
 
-                    var (knownTags, unknownTags) = _context.GetTags(tags);
-                    _context.Tags.AddRange(unknownTags);
-                    unknownTags.ForEach(tag => knownTags.Add(tag));
+                var (knownTags, unknownTags) = _context.GetTags(tags);
+                _context.Tags.AddRange(unknownTags);
+                unknownTags.ForEach(tag => knownTags.Add(tag));
 
-                    article.Tags
-                        .Where(tag => !knownTags.Contains(tag))
-                        .ToList()
-                        .ForEach(tag => article.Tags.Remove(tag));
-                    knownTags
-                        .Where(tag => !article.Tags.Contains(tag))
-                        .ToList()
-                        .ForEach(tag => article.Tags.Add(tag));
+                article.Tags
+                    .Where(tag => !knownTags.Contains(tag))
+                    .ToList()
+                    .ForEach(tag => article.Tags.Remove(tag));
+                knownTags
+                    .Where(tag => !article.Tags.Contains(tag))
+                    .ToList()
+                    .ForEach(tag => article.Tags.Add(tag));
 
-                    await _context.SaveChangesAsync();
-                    return RedirectToAction(
-                        nameof(IssuesController.Edit),
-                        "Issues",
-                        new { id = article.IssueSignature },
-                        id.ToString());
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ArticleExists(input.Id))
-                    {
-                        return NotFound();
-                    }
-
-                    throw;
-                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(
+                    nameof(IssuesController.Edit),
+                    "Issues",
+                    new { id = article.IssueSignature },
+                    id.ToString());
             }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArticleExists(input.Id))
+                {
+                    return NotFound();
+                }
 
-            return RedirectToAction("Index", "Issues");
+                throw;
+            }
         }
 
 #if false
